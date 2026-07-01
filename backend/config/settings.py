61fbaeb09b4851a0936f7a968fceb63243ab0f74
Atkags <atkamhos.snr@gmail.com>
2026-07-01 21:34:21 +0200

    Setup Django and React interaction

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'corsheaders',
+    'core',
+]
+
+CORS_ALLOWED_ORIGINS = [
+  "http://localhost:5173",
 ]
 
 MIDDLEWARE = [
@@ -47,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
 ]
 
 ROOT_URLCONF = 'config.urls'
